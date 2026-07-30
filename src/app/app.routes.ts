05c967c34e2a2374f9286a66bb4feaae5de31ed2
alfredoronald <choquerivealfredoronald@gmail.com>
2026-07-30 18:52:46 -0400

    fix: remove broken lazy route import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'home',
-    loadComponent: () =>
-      import('./features/home/home.component').then(m => m.HomeComponent),
-  },
-  { path: '**', redirectTo: 'home' },
-];
+export const routes: Routes = [];
