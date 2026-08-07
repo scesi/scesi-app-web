7d015e5d220f244096930a4d1981458b6e0a8c6a
alfredoronald <choquerivealfredoronald@gmail.com>
2026-08-06 22:21:23 -0400

    fix: add lazy loading routes and inline template for home feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
-import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<router-outlet />`,
 })
-export class App {
-  protected readonly title = signal('scesi-app-web');
-}
+export class App {}
